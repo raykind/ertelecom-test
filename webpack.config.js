c40eabf8bef36f134bb4b61e25921185d1a533de
Lev Ufimtsev <ufimtsev-la@dartit.ru>
2019-09-08 12:17:26 +0500

    refact

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ let conf = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: "main.js",
-    publicPath: "dist/"
+    publicPath: "/ertelecom-test/"
   },
   devServer: {
     overlay: true
